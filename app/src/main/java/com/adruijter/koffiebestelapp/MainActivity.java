6e36eae08f7e0b65214598cf24ac2a0ff7a96b4c
Arjan de Ruijter <rra@mboutrecht.nl>
2016-02-12 15:14:02 +0100

    App

--- app/src/main/java/com/adruijter/koffiebestelapp/MainActivity.java
+++ app/src/main/java/com/adruijter/koffiebestelapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.adruijter.koffiebestelapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void orderCoffee(View view)
+    {
+        EditText txtFirstname = (EditText) findViewById(R.id.firstname);
+        EditText txtTest = (EditText) findViewById(R.id.testText);
+        txtTest.setText("Voornaam is: " + txtFirstname.getText() + "\n" +
+                        "Wat nieuwe tekst");
     }
 }
