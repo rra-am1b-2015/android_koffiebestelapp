122e503dcd4f4f5c9c01d1f6e0c07f1be1aa933d
Arjan de Ruijter <rra@mboutrecht.nl>
2016-02-29 14:38:50 +0100

    Toevoegen gegevens

--- app/src/main/java/com/adruijter/koffiebestelapp/MainActivity.java
+++ app/src/main/java/com/adruijter/koffiebestelapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.adruijter.koffiebestelapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,12 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -25,16 +32,52 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSelectedDayChange(CalendarView view, int year,
                                             int month, int dayOfMonth) {
-                datumTest = dayOfMonth + "-" + month + 1 + "-" + year;
+                datumTest = dayOfMonth + "-" + (month + 1) + "-" + year;
                 orderCoffee(view);
                 // TODO Auto-generated method stub
                 Toast.makeText(
                         getBaseContext(),
-                        "Selected Date is\n\n" + dayOfMonth + " / " + month + 1
+                        "Selected Date is\n\n" + dayOfMonth + " / " + (month + 1)
                                 + " / " + year, Toast.LENGTH_LONG).show();
 
             }
         });
+
+        File file = getBaseContext().getFileStreamPath("myfile.txt");
+        String readString = "";
+        if (file.exists()) {
+
+            FileInputStream fis;
+
+            Toast.makeText(
+                    getBaseContext(),
+                    "Hij bestaat", Toast.LENGTH_LONG).show();
+
+            try {
+                fis = openFileInput("myfile.txt");
+
+                fis.read(readString.getBytes());
+                StringBuilder sb = new StringBuilder();
+                Reader r = new InputStreamReader(fis, "UTF-8");
+                int ch = r.read();
+                while ( ch >= 0)
+                {
+                    sb.append(ch);
+                    ch = r.read();
+                }
+
+                Toast.makeText(
+                        getBaseContext(),
+                        sb.toString(), Toast.LENGTH_LONG).show();
+                fis.close();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+        }
+        Toast.makeText(getBaseContext(), readString  , Toast.LENGTH_LONG).show();
     }
 
     public void orderCoffee(View view)
@@ -60,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
         {
             milkTxt += " geen ";
         }
-        milkTxt += "melk";
+        milkTxt += "melk" + getFilesDir();
 
 
         //Date datum = new Date(cld.getDate());
@@ -68,7 +111,22 @@ public class MainActivity extends AppCompatActivity {
 
 
         String output = String.format("Voornaam: %s \n Tussenvoegsel: %s \n Achternaam: %s \n Aantal Koffie: %s \n Wel of geen melk: %s \n Sterktepercentage: %s procent \n datum: %s",
-                                            txtFirstname.getText(), txtInfix.getText(),txtLastname.getText(), txtNumberOfCoffee.getText(), milkTxt, skbar.getProgress(), datumTest  );
+                txtFirstname.getText(), txtInfix.getText(), txtLastname.getText(), txtNumberOfCoffee.getText(), milkTxt, skbar.getProgress(), datumTest);
+
+        File file = this.getFilesDir();
+        File newFile = new File(file.getAbsolutePath() + "/arjan");
+        newFile.mkdir();
+        String filename = "myfile.txt";
+        String string = "Hello world!";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(output.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         txtTest.setText(output);
     }
 
